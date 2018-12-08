439b31e66b0a22cbd585528093ab35f2456d510b
msintaha <msintaha94@gmail.com>
2018-12-08 17:20:03 +0600

    Fix eslint errors

--- server.js
+++ server.js
@@ -1,48 +1,45 @@
-'use strict';
+const mongo = require('mongodb').MongoClient;
+const client = require('socket.io').listen(8080).sockets;
+const path = require('path');
+const express = require('express');
 
-const mongo = require('mongodb').MongoClient,
-	client = require('socket.io').listen(8080).sockets,
-	path = require('path'),
-	express = require('express'),
-	app = express();
+const app = express();
 
-app.listen(3000)
+app.listen(3000);
 app.use(express.static(path.join(__dirname, 'public')));
 
-mongo.connect('mongodb://127.0.0.1/chat', function (err, db) {
-	if (err) { throw err; }
-	client.on('connection', function (socket) {
-		const sendStatus = function (statusMsg) {
-			socket.emit('status', statusMsg);
-		};
-
-		getAllMessages(db, socket);
-
-		socket.on('input', function (data) {
-			const name = data.name;
-			const message = data.message;
-
-			let whitespacePattern = /^\s*$/;
-
-			if (whitespacePattern.test(name) ||
-				whitespacePattern.test(message)) {
-				sendStatus('Name and message is required');
-			} else {
-				db.collection('messages').insert({ name, message }, function () {
-					client.emit('output', [data]);
-					sendStatus({
-						message: 'Message sent',
-						clear: true
-					});
-				});
-			}
-		});
-	});
-});
-
-const getAllMessages = function (db, socket) {
-	db.collection('messages').find().limit(100).sort({ _id: 1 }).toArray(function (err, res) {
-		if (err) { throw err; }
-		socket.emit('output', res);
-	});
+const getAllMessages = (db, socket) => {
+  db.collection('messages').find().limit(100).sort({ _id: 1 })
+    .toArray((err, res) => {
+      if (err) { throw err; }
+      socket.emit('output', res);
+    });
 };
+
+mongo.connect('mongodb://127.0.0.1/chat', (err, db) => {
+  if (err) { throw err; }
+  client.on('connection', (socket) => {
+    const sendStatus = (statusMsg) => {
+      socket.emit('status', statusMsg);
+    };
+
+    getAllMessages(db, socket);
+
+    socket.on('input', (data) => {
+      const { name, message } = data;
+      const whitespacePattern = /^\s*$/;
+
+      if (whitespacePattern.test(name) || whitespacePattern.test(message)) {
+        sendStatus('Name and message is required');
+      } else {
+        db.collection('messages').insert({ name, message }, () => {
+          client.emit('output', [data]);
+          sendStatus({
+            message: 'Message sent',
+            clear: true,
+          });
+        });
+      }
+    });
+  });
+});
